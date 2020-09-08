fa897bc78256a0de655894219db710dba9883738
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-08 19:06:19 +0000

    add character count feature

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,14 @@ $(document).ready(() => {
   $('#tweet-text').on('keydown keyup paste', function() {
     let $counterEl = $(this).parent().find('.counter');
     let charCount = $(this).val().length;
+    let newClass
+
     $counterEl.text(140 - charCount);
     if (140 - charCount < 0) {
-      $counterEl.css('color', 'red');
+      // $counterEl.css('color', 'red');
+      $counterEl.addClass('exceedingCharLimit');
     } else {
-      
+      $counterEl.removeClass('exceedingCharLimit');
     }
   });
 });
