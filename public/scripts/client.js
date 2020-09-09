587ae2fbb84de9dfb4b46cd6af8be306b484b23d
Fatima Nguyen <fatima3455@hotmail.com>
2020-09-09 23:57:42 +0000

    format html and css of compose button;

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,19 +14,13 @@ $(document).ready(function() {
     const textArea = $(this).find('textarea').val();
 
     event.preventDefault();
-
     $errorMess.hide();
+
     if (!textArea) {
       $errorMess.text('This field is required.').show();
     } else if (textArea.length > 140) {
       $errorMess.text('The text entered exceeds the maximum length.').show();
     } else {
-      // $.ajax('/tweets/', {method: 'POST', data: $formText}) //ALTERNATIVE WAY
-      // $.get('/tweets/', (data) => {
-      //   const newTweet = createTweetElement(data[data.length - 1]);
-      //   $('#tweet-container').prepend(newTweet);
-      // })
-
       $.post('/tweets', $formText)
         .then(() => {
           return $.get('/tweets/')
